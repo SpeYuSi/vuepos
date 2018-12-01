000b3e628e6a09065139cae5f9c214a5531283d4
YU <yushuai1996@live.com>
2018-12-01 14:59:28 +0800

    initial commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-goumai"></i>
+                <div>收银</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+.left-nav{
+    color: #fff;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1d8ce0;
+    float: left;
+    width: 8%;
+}
+.iconfont{
+    font-size: 74px;
+
+}
+.left-nav ul{
+    padding: 0;
+    margin: 0;
+
+}
+.left-nav li{
+    list-style: none;
+    text-align:center;
+    border-bottom: 1px solid #20a0ff;
+    padding:10px;
+
+}
+</style>
